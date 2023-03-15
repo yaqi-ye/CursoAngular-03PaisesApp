daa14d50d77bd9925eddb39567f951d61b083bfd
Yaqi <yaqiye22@gmail.com_>
2023-03-15 21:46:10 +0100

    Revision

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 import { Country } from '../interfaces/pais.interface';
 
@@ -26,5 +26,17 @@ export class PaisService {
   getPaisPorAlpha( id: string ): Observable<Country>{
     const url = `${ this.apiUrl }/alpha/${ id }`;
     return this.http.get<Country>( url );
+  }
+
+  buscarRegion( region: string ): Observable<Country[]>{
+
+    const params = new HttpParams()
+          .set( 'fields', 'name,capital,alpha2code,flag,population');
+
+    const url = `${ this.apiUrl }/region/${ region }`;
+    return this.http.get<Country[]>( url, { params } )
+            .pipe(
+              tap(console.log)
+            );
   }
 }
